89f1aa457551dce1d2d64231282a3eca71c7122b
atemi72 <yuto67658@gmail.com>
2025-08-08 23:05:17 +0900

    Update scheduler.py
    今んとこregion_demand.pyが1日3回実行まで出来るから、AM1時&AM9時&PM5時にそれぞれ自動実行するように改変
    ほんでこっちのプログラムも実行時にSlackに通知来るようにした

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,34 @@
 import schedule
 import time
 from region_demand import main
+from datetime import datetime
+import pytz
+import requests
 
-#Schedule the task to run every day at 01:00 AM
-schedule.every().day.at("01:00").do(main)
+# Slack Webhook URL
+SLACK_WEBHOOK_URL = "Your Slack Webhook URL"
 
-print("Scheduler is runnning (Press ctrl+C to stop)")
+def send_slack_message(message):
+    try:
+        payload = {"text": message}
+        requests.post(SLACK_WEBHOOK_URL, json=payload)
+    except Exception as e:
+        print(f"Slack sending error: {e}")
+
+def run_main_with_notice():
+    jst = pytz.timezone("Asia/Tokyo")
+    now_str = datetime.now(jst).strftime("%Y-%m-%d %H:%M:%S")
+    send_slack_message(f"Started Scheduler running: {now_str}")
+    main()
+
+#Run three times a day
+schedule.every().day.at("01:00").do(run_main_with_notice)
+schedule.every().day.at("09:00").do(run_main_with_notice)
+schedule.every().day.at("17:00").do(run_main_with_notice)
+
+print("Scheduler is running (Press ctrl+C to stop)")
+send_slack_message("Scheduler is running")
+
 while True:
     schedule.run_pending()
     time.sleep(60)
